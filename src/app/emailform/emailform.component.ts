5a6caf4165eee8a923c1c1108018e763a8ba8f3b
AKSHAYAKN <123544052+AKSHAYAKN@users.noreply.github.com>
2024-10-21 14:05:37 +0530

    Add files via upload

---
+++ src/app/emailform/emailform.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emailform',
+  templateUrl: './emailform.component.html',
+  styleUrls: ['./emailform.component.css']
+})
+export class EmailformComponent {
+
+}
